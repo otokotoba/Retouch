8b7247ad7fabab82d872aeec3590b3666bff0fb1
otokotoba <otokotoba.music@gmail.com>
2024-09-26 19:38:27 +0900

    fix tests for the token trigger

--- tests/triggers/token-trigger.test.ts
+++ tests/triggers/token-trigger.test.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/unbound-method */
+import KeyvRedis from '@keyv/redis';
 import { expect } from 'chai';
-import { Locale } from 'discord.js';
+import dotenv from 'dotenv';
+import { Keyv } from 'keyv';
 import * as sinon from 'sinon';
 
 import { TriggerHandler } from '../../src/events/trigger-handler.js';
+import { Language } from '../../src/models/enum-helpers/language.js';
 import { EventData } from '../../src/models/internal-models.js';
 import { TokenTrigger } from '../../src/triggers/token-trigger.js';
 import { FormatUtils } from '../../src/utils/format-utils.js';
@@ -21,14 +24,16 @@ const URLS = [
 const TOKEN =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2V4YW1wbGUuYXV0aDAuY29tLyIsImF1ZCI6Imh0dHBzOi8vYXBpLmV4YW1wbGUuY29tL2NhbGFuZGFyL3YxLyIsInN1YiI6InVzcl8xMjMiLCJpYXQiOjE0NTg3ODU3OTYsImV4cCI6MTQ1ODg3MjE5Nn0.CA7eaHjIHz5NxeIJoFK9krqaeZrPLwmMmgI_XiQiIkQ';
 
-describe('TokenTrigger', () => {
+describe('Token trigger', () => {
+    dotenv.config();
+
     const discord = new DiscordMock();
-    const trigger = new TokenTrigger();
+    const trigger = new TokenTrigger(new Keyv(new KeyvRedis(process.env.REDIS_URL)));
 
     beforeEach(() => {
         sinon
             .stub(discord.eventDataService, 'create')
-            .resolves(new EventData(Locale.Japanese, Locale.Japanese));
+            .resolves(new EventData(Language.Default, Language.Default));
     });
 
     afterEach(() => {
