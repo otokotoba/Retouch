1aa99c2790dcbb0877a568cb8017dbb9f6302388
otokotoba <otokotoba.music@gmail.com>
2024-09-27 23:19:13 +0900

    add tests for the "settings log-channel" command

---
+++ tests/commands/settings-command.test.ts
@@ -0,0 +1,55 @@
+import KeyvRedis from '@keyv/redis';
+import { expect } from 'chai';
+import dotenv from 'dotenv';
+import { Keyv } from 'keyv';
+import * as sinon from 'sinon';
+
+import { SettingsCommand } from '../../src/commands/chat/settings-command.js';
+import { Settings } from '../../src/models/database.js';
+import { Language } from '../../src/models/enum-helpers/index.js';
+import { EventData } from '../../src/models/internal-models.js';
+import { InteractionUtils } from '../../src/utils/index.js';
+import { DiscordMock } from '../discord-mock.js';
+
+describe('Settings command', () => {
+    dotenv.config();
+
+    const discord = new DiscordMock();
+    const db = new Keyv(new KeyvRedis(process.env.REDIS_URL));
+    const command = new SettingsCommand(db);
+
+    beforeEach(() => {
+        sinon
+            .stub(discord.eventDataService, 'create')
+            .resolves(new EventData(Language.Default, Language.Default));
+    });
+
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it('should save the channel id on database when the sub command is log-channel.', async () => {
+        const [channelId, guildId] = ['1', '1'];
+
+        sinon
+            .stub(discord.chatInputCommandInteraction.options, 'getSubcommand')
+            .returns('log-channel');
+        sinon
+            .stub(discord.chatInputCommandInteraction.options, 'getChannel')
+            .returns(discord.textChannel);
+
+        sinon.stub(discord.textChannel, 'id').get(() => channelId);
+        sinon.stub(discord.chatInputCommandInteraction, 'guildId').get(() => guildId);
+
+        sinon.stub(InteractionUtils, 'send').resolves();
+
+        await command.execute(
+            discord.chatInputCommandInteraction,
+            await discord.eventDataService.create()
+        );
+
+        const settings = await db.get<Settings>(guildId);
+
+        expect(settings?.logChannel).to.be.equals(channelId);
+    });
+});
--- tests/discord-mock.ts
+++ tests/discord-mock.ts
@@ -1,22 +1,46 @@
-import { APIMessage, APIUser, Client, Message, User } from 'discord.js';
+import {
+    APIMessage,
+    APIUser,
+    ChannelType,
+    ChatInputCommandInteraction,
+    Client,
+    Guild,
+    Message,
+    Options,
+    TextChannel,
+    User,
+} from 'discord.js';
 
 import { EventDataService } from '../src/services/event-data-service.js';
+import '../src/models/dotenv.js';
 
 export class DiscordMock {
-    public client!: Client;
-    public user!: User;
-    public message!: Message;
-    public eventDataService!: EventDataService;
+    public client: Client;
+    public user: User;
+    public message: Message;
+    public eventDataService: EventDataService;
+    public chatInputCommandInteraction: ChatInputCommandInteraction;
+    public guild: Guild;
+    public textChannel: TextChannel;
 
     constructor() {
         this.mockClient();
         this.mockUser();
         this.mockMessage();
         this.mockEventDataService();
+        this.mockChatInputCommandInteraction();
+        this.mockGuild();
+        this.mockTextChannel();
     }
 
     private mockClient(): void {
-        this.client = new Client({ intents: [] });
+        this.client = new Client({
+            intents: [],
+            // necessary for this.mockTextChannel
+            makeCache: Options.cacheWithLimits({
+                ...Options.DefaultMakeCacheSettings,
+            }),
+        });
     }
 
     private mockUser(): void {
@@ -43,5 +67,27 @@ export class DiscordMock {
 
     private mockEventDataService(): void {
         this.eventDataService = new EventDataService();
+    }
+
+    private mockChatInputCommandInteraction(): void {
+        this.chatInputCommandInteraction = Reflect.construct(ChatInputCommandInteraction, [
+            this.client,
+            {
+                user: this.user,
+                data: { id: '1' },
+            },
+        ]);
+    }
+
+    private mockGuild(): void {
+        this.guild = Reflect.construct(Guild, [this.client, {}]);
+    }
+
+    private mockTextChannel(): void {
+        this.textChannel = Reflect.construct(TextChannel, [
+            this.guild,
+            { type: ChannelType.GuildText },
+            this.client,
+        ]);
     }
 }
